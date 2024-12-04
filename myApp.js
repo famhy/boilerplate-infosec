0e444db2a2e8cdad460c2b84202ae40b4101ed26
fahmi <fahmidouiri@gmail.com>
2024-12-04 10:46:10 +0100

    hidePoweredBy

--- myApp.js
+++ myApp.js
@@ -3,7 +3,10 @@ const helmet = require("helmet");
 const app = express();
 
 
-app.use(helmet());
+app.use(helmet({
+  xPoweredBy: false,
+}));
+helmet.hidePoweredBy()
 
 
 
@@ -48,15 +51,17 @@ app.use(helmet());
 
 
 
-module.exports = app;
 const api = require('./server.js');
+console.log(api);
+
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
+module.exports = app;
