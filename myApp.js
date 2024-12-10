0ab7489bb3a69373519de05845aa4c601ff13390
fahmi <fahmidouiri@gmail.com>
2024-12-10 16:15:46 +0100

    hidePoweredBy

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ const app = express();
 
 
 app.use(helmet());
-app.use(helmet.hidePoweredBy())
+ app.use(helmet.hidePoweredBy({
+  setTo: "PHP 4.2.0",
+}))
 
 
 
@@ -58,6 +60,12 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
+app.get("/test", function (request, response) {
+  response.send("Test");
+});
+
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
